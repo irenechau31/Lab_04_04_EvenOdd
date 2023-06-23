d1ec7307800bfe8d5b6b5c41fefec8acf7febb04
irenechau31 <beasthoang01@gmail.com>
2023-06-23 13:21:43 +0700

    INITIAL COMMIT

---
+++ src/OddEven.java
@@ -0,0 +1,11 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int numExamine = 2;
+        if (numExamine % 2 == 0) {
+            System.out.println(numExamine + " is even");
+        } else {
+            System.out.println(numExamine + " is odd");
+        }
+
+    }
+}
